1be0fc6b13dd6b39a84893ad8ae8d57bef455f7c
liamnordvall06-source <liamnordvall06@gmail.com>
2026-01-05 12:51:36 +0100

    Changed invalid commit

--- addio/src/pages/configurePage.js
+++ addio/src/pages/configurePage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import styles from "./configurePage.module.css";
 import SidebarComponent from "../components/sidebarComponent";
-import STLViewerComponent from "../components/STLViewerComponent";
 
 
 const ConfigurePage = () => {
     return (
         <div className={styles.mainPage}>
-            <STLViewerComponent />
+            <SidebarComponent />
         </div>
     );
 }
